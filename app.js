2ce3cb6c34024f1b7c6d01b439ab33d0abb38cfa
Din S Ho <dinsho@Din-S-M1.local>
2022-11-28 22:27:26 -0800

    db listed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const Picblog = require('./models/picblog');
+const cities = require('./seeds/cities')
 
 const mongoose = require('mongoose');
 
@@ -27,6 +28,23 @@ app.get('/picblogs', async (req, res) => {
     const picblogs = await Picblog.find({});
     res.render('picblogs/index', { picblogs })
 });
+
+// app.get('/makepicblog', async (req, res) => {
+//     for (let i = 0; i < 50; i++) {
+//         const random1000 = Math.floor(Math.random() * 1000);
+//         const pic = new Picblog({
+//             location: `${cities[random1000].city}, ${cities[random1000].state}`
+//         })
+//         await pic.save();
+//         res.send(pic);
+//     }
+    // const random1000 = Math.floor(Math.random() * 1000);
+    // const pic = new Picblog({
+    //     title: 'Califonia'
+    // })
+    // await pic.save();
+    // res.send(pic);
+// })
 
 app.listen(3000, () => {
     console.log('Live on 3000')
--- seeds/index.js
+++ seeds/index.js
@@ -13,20 +13,28 @@ mongoose.connect('mongodb://127.0.0.1/pic-blog', {
     }
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
-
-const seedDB = async () => {
-    await Picblog.deletemany({});
+app.get('/makepicblog', async (req, res) => {
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const pic = new Picblog({
-            location: `${cities[random1000].city, $cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
         })
         await pic.save();
     }
-}
-
-seedDB().then(() => {
-    mongoose.connection.close();
 })
+
+// const seedDB = async () => {
+//     await Picblog.deletemany({});
+//     for (let i = 0; i < 50; i++) {
+//         const random1000 = Math.floor(Math.random() * 1000);
+//         const pic = new Picblog({
+//             location: `${cities[random1000].city}, ${cities[random1000].state}`
+//         })
+//         await pic.save();
+//     }
+// }
+
+// seedDB().then(() => {
+//     mongoose.connection.close();
+// })
 
